c4e03616fdac71a2e66ed2fb20f677815d162520
Alex Rock Ancelet <pierstoval@gmail.com>
2016-11-19 15:35:15 +0100

    Updated frontend JS script to test websockets (it works !!! <3 )

---
+++ assets/js/dependencies/front_main.js
@@ -0,0 +1,33 @@
+(function(w, d, io){
+
+    io.socket.post('/s/register_home', {}, function(body, response) {
+        if (200 !== response.statusCode) {
+            console.log('Server responded with status code ' + response.statusCode + ' and data: ', body);
+
+            d.getElementById('content').innerHTML +=
+                'Failed to register to home socket.<br>'
+                + 'Server responded with status code '
+                + response.statusCode
+                + ' and data:'
+                + '<pre>'
+                + JSON.stringify({body: body, response: response}, null, 4)
+                + '</pre>';
+
+            return;
+        }
+
+        d.getElementById('content').innerHTML +=
+            'Response OK! :D.<br>'
+            + 'Server responded with status code '
+            + response.statusCode
+            + ' and data:'
+            + '<pre>'
+            + JSON.stringify({body: body, response: response}, null, 4)
+            + '</pre>';
+
+
+        io.socket.on('home', console.info);
+        io.socket.on('hello', console.info);
+    });
+
+})(window, window.document, io);
