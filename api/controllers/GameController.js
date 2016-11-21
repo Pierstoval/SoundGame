8ccbea2c7d2133404673b12cba021eea829a625f
Alex Rock Ancelet <pierstoval@gmail.com>
2016-11-21 22:51:52 +0100

    Handle key events in backend

--- api/controllers/GameController.js
+++ api/controllers/GameController.js
@@ -1,3 +1,11 @@
+
+const keysAssociations = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+};
+
 module.exports = {
     socketRegister: function (req, res) {
         if (!req.isSocket) {
@@ -15,6 +23,26 @@ module.exports = {
         }
 
         GameUsers.removeUserBySocketId(req.socket.id);
+
+        return res.ok();
+    },
+
+    socketKeyUp: function (req, res) {
+        if (!req.isSocket) {
+            return res.badRequest();
+        }
+
+        console.info('Key up', req.allParams());
+
+        return res.ok();
+    },
+
+    socketKeyDown: function (req, res) {
+        if (!req.isSocket) {
+            return res.badRequest();
+        }
+
+        console.info('Key down', req.allParams());
 
         return res.ok();
     },
