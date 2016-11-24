f32ca22d72a50670c4329da91b18e13c6e6bf64e
Alex Rock Ancelet <pierstoval@gmail.com>
2016-11-24 13:53:34 +0100

    Add more data to serializer (pick image, sound notes and sounds to play)

--- api/services/Serializer.js
+++ api/services/Serializer.js
@@ -1,11 +1,35 @@
 module.exports = {
-    serializeUser: function (user) {
+    /**
+     * This corresponds to the data sent to the client.
+     * Serialization this way is good to have the less possible amount of data.
+     *
+     * @param user
+     */
+    serializeDataFromUser: function (user) {
         return {
             x: user.pick.x,
             y: user.pick.y,
-            r: user.pick.r,
+            r: user.pick.radius,
             a: user.pick.angle,
             s: user.pick.speed,
+            i: user.pick.imageUrl,
+            n: this.serializeNotesArray(user.level.notes),
+            snd: user.playSounds,
         };
-    }
+    },
+
+    serializeNotesArray: function(notes) {
+        var serializedNotes = [];
+
+        for (var i = 0, l = notes.length; i < l; i++) {
+            let note = notes[i];
+            serializedNotes.push({
+                x: note.x,
+                y: note.y,
+                i: note.imageUrl,
+            });
+        }
+
+        return serializedNotes;
+    },
 };
